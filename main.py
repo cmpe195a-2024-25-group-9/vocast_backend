e39cd1f877254aca3a53597a7fa8f10dfd809571
Fardin Haque <fardinhaque60@gmail.com>
2025-05-03 16:38:33 -0700

    added middleware to allow requests from frontend

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import threading
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or ["*"] for all origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 stream_thread = None
 stop_flag = threading.Event()
